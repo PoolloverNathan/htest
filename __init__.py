bd1c58f4cf17629cc9c68b5aa621ae29ad4051a0
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-03-10 09:36:36 -0400

    Create route /page/<id>
    Inside __init__.py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,16 @@ import sys, os
 from flask import *
 app = Flask(__name__)
 
+@app.route("/page/<id>", methods=["GET"])
+def getpage(id):
+  try:
+    with f as open(f"pages/<id>", "r"):
+      f.seek(0,2)
+      s = f.tell()
+      f.seek(0,0)
+      return f.read(s)
+  except OSError:
+    return "", 404
+
 if __name__ == "__main__":
   app.run(port=os.environ.get("PORT", 3000))
